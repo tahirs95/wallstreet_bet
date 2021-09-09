6bc6ebc071670275e9d6dc05ff2d83f9baaa8d7e
tahirs95 <tahirs95@hotmail.com>
2021-09-09 17:55:30 +0500

    code refactored.

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 from flask import Flask,render_template,request
 import pandas as pd
-from psaw import PushshiftAPI
 import datetime as dt
 import time
 from collections import Counter
@@ -11,18 +10,12 @@ from datetime import timedelta
 import functools, operator
 import os
 
-
-
 app = Flask(__name__)
-
-@app.route('/')
-
 
 @app.route('/', methods=['GET','POST'])
 def index():
     if request.method == 'GET':
  
-
          filename = dt.datetime.today().strftime("%d %m %Y")+".csv"
      
          data = pd.read_csv(filename)
@@ -30,15 +23,11 @@ def index():
          currname=[]
          counts=[]
          for i in data.values:
-            #print(i)
             if i[1] != 0:
                 currname.append(i[0])
                 counts.append(i[1])
         
-        # print(currname)
-        # print(counts)
          dictionary = dict(zip(currname, counts))   
-         #print(dictionary)
 
          return render_template('index.html',dict=dictionary)
 
@@ -54,16 +43,11 @@ def yesterday():
         currname=[]
         counts=[]
         for i in data.values:
-            #print(i)
             if i[1] != 0:
                 currname.append(i[0])
                 counts.append(i[1])
         
-        # print(currname)
-        # print(counts)
         dictionary = dict(zip(currname, counts))   
-         #print(dictionary)
-
         return render_template('yesterday.html',dict=dictionary)
 
  
